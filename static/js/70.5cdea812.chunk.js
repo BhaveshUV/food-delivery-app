ee45bc2e9456cb4dfde7f2631e095e5c8ec35bad
Bhavesh Vishwakarma <bhaveshruv@gamil.com>
2024-03-26 20:53:21 +0530

    Updates

--- static/js/70.5cdea812.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkfood_delivery_app=self.webpackChunkfood_delivery_app||[]).push([[70],{70:(e,t,o)=>{o.r(t),o.d(t,{default:()=>l});var s=o(579);const l=()=>(0,s.jsxs)("div",{className:"text-white text-center",children:[(0,s.jsx)("h1",{className:"text-yellow-500 text-3xl font-bold",children:"Welcome to our grocery store\ud83d\ude4c\u2728"}),"We have a lot of child components inside this webpage."]})}}]);
-//# sourceMappingURL=70.5cdea812.chunk.js.map
